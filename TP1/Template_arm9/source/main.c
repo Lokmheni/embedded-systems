7f11d8c7782ec46b210358196c3d187e4a73c93c
Simon Thür <thuer.simon@hotmail.com>
2022-10-02 18:54:16 +0200

    print arrayz i guess

--- TP1/Template_arm9/source/main.c
+++ TP1/Template_arm9/source/main.c
@@ -6,10 +6,18 @@
 #include <nds.h>
 #include <stdio.h>
 
+
+int array[] = {0,1,2,3,4,5,6,7,8,9};
+
+
 int main(void) {
 	
     consoleDemoInit();
-    printf("\nHello World Did you ever hear the tragedy of darth plageious the wise!?\n");
+    for (int i = 0; i < 10; i++)
+    {
+        printf("Number %d", array[ i ] );
+    }
+
 
     while(1)
         swiWaitForVBlank();	
