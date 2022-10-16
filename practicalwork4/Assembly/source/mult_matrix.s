8c45bd727b4bef5b64d5429c247d4c4a0030f709
Simon Thür <thuer.simon@hotmail.com>
2022-10-16 19:33:04 +0200

    fix SP problem

--- practicalwork4/Assembly/source/mult_matrix.s
+++ practicalwork4/Assembly/source/mult_matrix.s
@@ -6,19 +6,18 @@
 
 MultMatrix:
 
-stmdb sp!, {r4 - r15, lr}
+stmdb sp!, {r4 - r12, lr}
 
 mov r4,#0 @r4 = i=0
-mov r5,#0 @r5 = j=0
-mov r6,#0 @r6 = k=0
-
 fori:
     cmp r4,r2 @i<len
     bge endfori
 
+
     mul r10,r4,r2 @r10 = len * i
 
 
+    mov r5,#0 @r5 = j=0
     forj:
         cmp r5,r2
         bge endfoj
@@ -28,15 +27,16 @@ fori:
         mov r7,#0 @clear tmp sum
 
 
+        mov r6,#0 @r6 = k=0
         fork:
             cmp r6,r2
             bge endfork
             
             mul r12,r6,r2 @r12 = len * k
-            add r13,r10,r6
-            ldr r8,[r0,r13,lsl #2] @A_ik
-            add r13, r12,r11
-            ldr r9,[r1,r13,lsl#2] @B_kj
+            add r8,r10,r6
+            ldr r8,[r0,r8,lsl #2] @A_ik
+            add r9, r12,r11
+            ldr r9,[r1,r9,lsl#2] @B_kj
 
             mul r9,r8 @A_ik * B_kj
             add r7,r9
@@ -47,8 +47,8 @@ fori:
         endfork:
 
     @now we store result into C_ij
-    add r13,r4,r11
-    str r6,[r3,r13,lsl #2]
+    add r8,r4,r11
+    str r6,[r3,r8,lsl #2]
 
     add r5,#1
     b forj
@@ -59,6 +59,6 @@ add r4,#1
 b fori
 endfori:
 
-ldmia sp!, {r4 - r15, lr}
+ldmia sp!, {r4 - r12, lr}
 bx lr
 .END
