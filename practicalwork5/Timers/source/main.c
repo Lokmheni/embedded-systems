b12ad27c585c79f4e1446ce3ae157e6ef5d09b77
Simon Thür <thuer.simon@hotmail.com>
2022-10-25 22:12:25 +0200

    ex 1 and 2

--- practicalwork5/Timers/source/main.c
+++ practicalwork5/Timers/source/main.c
@@ -6,65 +6,81 @@
 #include "chrono_display.h"
 
 
+extern int iSqrt( int );
+
 // Exercises 2 and 3
 int min, sec, msec;
 
+void Timer0()
+{
+    if ( ++msec >= 1000 )
+        msec = 0;
+
+    updateChronoDisp_Main( min, sec, msec );
+}
+void Timer1()
+{
+    if ( ++sec >= 60 )
+        {
+            sec = 0;
+            min++;
+        }
+}
 
 int main( void )
 {
     consoleDemoInit();
-    /*
-        //Exercise 1
 
-        u16 start, end;
-        int result;
+    // Exercise 1
 
-        // Initialize the timer with the correct configuration
-        TIMER0_CR = ...
+    u16 start, end;
+    int result;
 
-        // Set the TIMER_DATA to 0 (the timer starts to count in 0)
-        TIMER_DATA(0) = 0;
+    // Initialize the timer with the correct configuration
+    TIMER0_CR = TIMER_ENABLE | TIMER_DIV_1 | TIMER_IRQ_REQ;
 
-        //Read the value of the timer
-        start = ...
-        result = floor(sqrt((double)start));
-        //Read the value of the timer again
-        end = ...
-        //Print the solution
-        printf(...
+    // Set the TIMER_DATA to 0 (the timer starts to count in 0)
+    TIMER_DATA( 0 ) = 0;
 
-        //Do the same fot the hand-written square root
-        start = ...
-        result = iSqrt(start);
-        end = ...
-        printf(...
-    */
+    // Read the value of the timer
+    start  = TIMER0_DATA;
+    result = floor( sqrt( ( double ) start ) );
+    // Read the value of the timer again
+    end    = TIMER0_DATA;
+    // Print the solution
+    printf( "double sqrt = %fus\n", 29.838276541e-3 * ( end - start ) );
+
+    // Do the same fot the hand-written square root
+    start  = TIMER0_DATA;
+    result = iSqrt( start );
+    end    = TIMER0_DATA;
+    printf( "iSqrt = %fus\n", 29.838276541e-3 * ( end - start ) );
 
 
     // Exercise 2
-    /*
-        //Initialization of the display
-        initChronoDisp_Main();
 
-        //Initialize the interrupts correctly
-        ...
+    // Initialization of the display
+    initChronoDisp_Main();
 
-        //Set the seconds and mseconds to 0
-        sec = msec = 0;
+    // Initialize the interrupts correctly
+    irqInit();
 
-        //Configure two timers (i.e.: TIMER0 and TIMER1) correcly
-        TIMER_DATA(0) = ...
-        TIMER0_CR = ...
-        TIMER_DATA(1) = ...
-        TIMER1_CR = ...
+    // Set the seconds and mseconds to 0
+    min = sec = msec = 0;
 
-        //Associate the interrupt handlers
-        ...
-        ...
-        //Enable the interrupt lines of the timers
-        ...
-        ...
-    */
+    // Configure two timers (i.e.: TIMER0 and TIMER1) correcly
+    TIMER_DATA( 0 ) = TIMER_FREQ_1024( 1000 );
+    TIMER0_CR       = TIMER_ENABLE | TIMER_DIV_1024 | TIMER_IRQ_REQ;
+    TIMER_DATA( 1 ) = TIMER_FREQ_1024( 1 );
+    TIMER1_CR       = TIMER_ENABLE | TIMER_DIV_1024 | TIMER_IRQ_REQ;
+
+    // Associate the interrupt handlers
+    irqSet( IRQ_TIMER0, Timer0 );
+    irqSet( IRQ_TIMER1, Timer1 );
+    // Enable the interrupt lines of the timers
+    irqEnable( IRQ_TIMER0 );
+    irqEnable( IRQ_TIMER1 );
+
 
     /*
         //Exercise 3 (Comment exercise 1 and 2)
