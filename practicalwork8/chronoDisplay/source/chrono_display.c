5f4c5eac46afd25a2759a8719a19205ea5cf636f
Simon Thür <thuer.simon@hotmail.com>
2022-11-14 16:02:50 +0100

    one number

--- practicalwork8/chronoDisplay/source/chrono_display.c
+++ practicalwork8/chronoDisplay/source/chrono_display.c
@@ -1,20 +1,20 @@
 #include "chrono_display.h"
 
+#include <nds/arm9/background.h>
+
 void printDigit(u16* map, int number, int x, int y)
 {
-
+    int i, j;
+    for (i = 0; i < 4; i++)
+        {
+            for (j = 0; j < 8; j++)
+                {
+                    map[(32 * (y + j)) + (x + i)] = 32 * number + (4 * j) + i;
+                }
+        }
 }
 
-void updateChronoDisp(u16* map,int min, int sec, int msec)
-{
-
-}
+void updateChronoDisp(u16* map, int min, int sec, int msec) {}
 
 
-
-
-void changeColorDisp(uint16 b, uint16 c)
-{
-
-}
-
+void changeColorDisp(uint16 b, uint16 c) {}
--- practicalwork8/chronoDisplay/source/main.c
+++ practicalwork8/chronoDisplay/source/main.c
@@ -1,38 +1,49 @@
 /*---------------------------------------------------------------------------------
 
-	Basic template code for starting a DS app
+    Basic template code for starting a DS app
 
 ---------------------------------------------------------------------------------*/
 #include <nds.h>
+#include <nds/arm9/background.h>
+#include <nds/arm9/video.h>
 #include <stdio.h>
+
 #include "chrono_display.h"
+#include "numbers.h"
 
+
+#define MY_TILE_BASE 1
+#define MY_MAP_BASE  0
 //---------------------------------------------------------------------------------
-int main(void) {
-//---------------------------------------------------------------------------------
+int main(void)
+{
+    //---------------------------------------------------------------------------------
 
-	//Enable a suitable VRAM block and map it to the main engine
-//	VRAM_A_CR = ...
-	//Configure the engine in Mode 0 and use the BG0
-//	REG_DISPCNT = ...
-	//Configure the engine to be used as a 32x32 grid of tiles of 256 colors
-	//Use BG_MAP_BASE(0) and  a suitable BG_TILE_BASE
-//	BGCTRL[0] = ...
+    // Enable a suitable VRAM block and map it to the main engine
+    VRAM_A_CR   = VRAM_ENABLE | VRAM_A_MAIN_BG;
+    // Configure the engine in Mode 0 and use the BG0
+    REG_DISPCNT = MODE_0_2D | DISPLAY_BG0_ACTIVE;
 
-	//Copy the tiles and the palette to the corresonding location
-//	swiCopy(...
-//	swiCopy(...
 
-	//The digit 8 is printed in the upper left part of the screen
-	printDigit(BG_MAP_RAM(0), 8,0,0);
+    // Configure the engine to be used as a 32x32 grid of tiles of 256
+    // colors Use BG_MAP_BASE(0) and  a suitable BG_TILE_BASE
+    BGCTRL[0] = BG_32x32 | BG_COLOR_256 | BG_MAP_BASE(MY_MAP_BASE) |
+                BG_TILE_BASE(MY_TILE_BASE);
 
-	//The value 12:34.567 is going to be printed in the center of the screen
-	updateChronoDisp(BG_MAP_RAM(0),12,34,567);
+    // Copy the tiles and the palette to the corresonding location
+    dmaCopy(numbersTiles, BG_TILE_RAM(MY_TILE_BASE), numbersTilesLen);
+    dmaCopy(numbersPal, BG_PALETTE, numbersPalLen);
+    // The digit 8 is printed in the upper left part of the screen
+    printDigit(BG_MAP_RAM(0), 8, 0, 0);
 
-	//The background color is ste to BLACK and the digits color is set to RED
-	changeColorDisp(BLACK, RED);
+    // The value 12:34.567 is going to be printed in the center of the screen
+    updateChronoDisp(BG_MAP_RAM(0), 12, 34, 567);
 
-	while(1) {
-		swiWaitForVBlank();
-	}
+    // The background color is ste to BLACK and the digits color is set to RED
+    changeColorDisp(BLACK, RED);
+
+    while (1)
+        {
+            swiWaitForVBlank();
+        }
 }
