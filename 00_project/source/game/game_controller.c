ee977fd130ce819a59ab26477fb3c21d830f4588
Simon Thür <thuer.simon@hotmail.com>
2022-12-04 11:44:39 +0100

    add game_controller.c and rename typo in .h file

---
+++ 00_project/source/game/game_controller.c
@@ -0,0 +1,18 @@
+/**
+ * @file game_controller.c
+ * @author Simon Thür
+ * @brief
+ * @version 0.1
+ * @date 2022-12-04
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+
+#include <nds.h>
+
+#include "game_conrtoller.h"
+
+bool update_game() {}
+void reset_game() { return false; }
