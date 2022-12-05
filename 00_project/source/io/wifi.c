d4836968a8d6b8613c9448059ff2398a282fdf0a
Simon Thür <thuer.simon@hotmail.com>
2022-12-05 23:14:09 +0100

    wifi init/stop and package definition

--- 00_project/source/io/wifi.c
+++ 00_project/source/io/wifi.c
@@ -1,7 +1,7 @@
 /**
  * @file wifi.c
  * @author Simon Thür and Lokman Mheni
- * @brief
+ * @brief Control WIFI through packets of 4 bytes
  * @version 0.1
  * @date 2022-12-02
  *
@@ -13,7 +13,47 @@
 #include "wifi.h"
 
 
-void send_status(PlayerState* const plr) {}
+// used to detect changes (to then only send that which has changed)
+Player old_plr_state;
+
+
+typedef enum MsgType
+{
+    WIFI_SYNC,
+    WIFI_STOP,
+    WIFI_PLAYER_X_DIR_SPEED,
+    WIFI_PLAYER_Y_YS_ACTION,
+    WIFI_DAMAGE_X_Y_DMG,
+} MsgType;
+
+
+typedef struct WifiMsg
+{
+    MsgType msg;
+    u8      dat1;
+    u8      dat2;
+    u8      dat3;
+} WifiMsg;
+
+
+bool wifi_connect_network()
+{
+    // WiFi initialization
+    if (!initWiFi())
+        return false;
+    // Socket initialization
+    if (!openSocket())
+        return false;
+    return true;
+}
+void wifi_disconnect_network()
+{ // Close the Socket
+    void closeSocket();
+    // Disconnect the WiFi
+    void disconnectFromWiFi();
+}
+
+void send_status(Player* const plr) {}
 void send_damage(int dmg_x, int dmg_y) {}
 
 void receive_status(PlayerState* plr, bool* damage, int* dmg_x, int* dmg_y) {}
--- 00_project/source/io/wifi.h
+++ 00_project/source/io/wifi.h
@@ -13,18 +13,19 @@
 #ifndef __WIFI_H
 #define __WIFI_H
 
-
 #include "../game/game.h"
+#include "WiFi_minilib.h"
 
 
 // wifi usability shit
-void send_status( PlayerState* const plr );
-void send_damage( int dmg_x, int dmg_y );
+void send_status(Player* const plr);
+void send_damage(int dmg_x, int dmg_y);
 
-void receive_status( PlayerState* plr, bool* damage, int* dmg_x, int* dmg_y );
+void receive_status(PlayerState* plr, bool* damage, int* dmg_x, int* dmg_y);
 
 
 // wifi setup and control shit
 bool wifi_connect_network();
+void wifi_disconnect_network();
 
-#endif  // __WIFI_H
+#endif // __WIFI_H
