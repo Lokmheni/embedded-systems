9f85a8371066cc05d60eab10a80eeb8ca95dc4ce
Simon Thür <thuer.simon@hotmail.com>
2022-12-05 16:23:13 +0100

    improve game_controller header interface

--- 00_project/source/game/game_controller.h
+++ 00_project/source/game/game_controller.h
@@ -12,9 +12,31 @@
 
 #ifndef __GAME_CONTROLLER_H
 #define __GAME_CONTROLLER_H
+#include <nds.h>
 
-// true if game done
-bool update_game();
+/**
+ * @brief Execute next step of the game (for now, we assume that it also takes
+ * care of bluetooth transmission)
+ *
+ * @return int where 0 means the game continues
+ *                   1 means player_local has won round
+ *                   2 means player_remote has won round
+ *                   3 means the connection died
+ */
+int update_game();
+
+/**
+ * @brief Set/Reset the game stage, points health etc.
+ *
+ */
 void reset_game();
 
-#endif  // __GAME_CONTROLLER_H
+
+/**
+ * @brief Start new round, only resets health and positions. Ideally one should
+ * change background when calling this function
+ *
+ */
+void new_round();
+
+#endif // __GAME_CONTROLLER_H
