33bab9f31f1f1d0eefd68e8e6e4a293ea5350f2a
Simon Thür <thuer.simon@hotmail.com>
2022-12-07 10:04:19 +0100

    add howto for wifi

--- 00_project/source/io/wifi.h
+++ 00_project/source/io/wifi.h
@@ -1,11 +1,42 @@
 /**
  * @file wifi.h
  * @author Simon Thür and Lokman Mheni
- * @brief
+ * @brief WIFI interface
  * @version 0.1
  * @date 2022-12-02
  *
  * @copyright Copyright (c) 2022
+ *
+ *
+ * USAGE of wifi:
+ * 1)   When using wifi.h inteface, it is up to the user to first call @ref
+ *      wifi_connect_network() to connect to the wifi and set up a port.
+ *
+ * 2)   After the wifi has been enabled and one wants to play with other
+ *      players, it is vital to announce this using @ref announce_lfg(). One can
+ *      optionally send this message periodically, but it must be sent at least
+ *      once upon connecting to the network.
+ *
+ * 3)   The user is responsible for periodically polling incoming messages with
+ *      @ref receive_messages(). Upon receiving an LFG msg, the receiver becomes
+ *      the `master` and is in charge of syncing game (with commands such as
+ *      RESET_GAME and IS_PLAY defined in @ref wifi_instr.h)
+ *
+ * 4)   During game, for every frame send !LOCAL! player  with @ref
+ *      send_status(Player* const plr) and the function will arbitrate whether
+ *      or not a transmission needs to be sent or not.
+ *
+ * 5)   Whenever a player does damage, this is to be sent to the remote NDS
+ *      using @ref send_damage(u8 dmg_x, u8 dmg_y, u8 dmg)
+ *
+ * 6)   During the game, it is important to periodically poll for incoming
+ *       messages (again with @ref receive_messages()) and handle the (player,
+ *      damage or abort) instructions.
+ *
+ * 7)   Whoever loses the game (health, giveup, whatever) is in charge of
+ *      signalling the lose and becomes the new master, tasked with setting up a
+ *      new round etc.
+ *
  *
  */
 
@@ -21,7 +52,7 @@
 typedef enum MsgType
 {
     WIFI_SYNC_INSTR_SCORE,
-    WIFI_POLLY_PLAYERS,
+    WIFI_POLL_PLAYERS,
     WIFI_PLAYER_X_DIR_ACTION,
     WIFI_PLAYER_Y_YS_HP,
     WIFI_DAMAGE_X_Y_DMG,
@@ -68,6 +99,9 @@ void send_damage(u8 dmg_x, u8 dmg_y, u8 dmg);
  *         size 0, e.g. no messages received)
  */
 bool receive_messages(WifiMsg* rec);
+
+void send_ctrl_instruction(u8 instruction, u8 score);
+void announce_lfg();
 
 
 // wifi setup and control shit
