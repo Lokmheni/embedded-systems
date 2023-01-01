06f34d5ab9268ecf4951aa645065e6ad6f670fde
Simon Thür <thuer.simon@hotmail.com>
2023-01-01 18:27:16 +0100

    doxy MD format

--- 00_project/source/io/wifi.h
+++ 00_project/source/io/wifi.h
@@ -9,35 +9,35 @@
  *
  *
  * USAGE of wifi:
- * 1)   When using wifi.h inteface, it is up to the user to first call @ref
+ * 1.   When using wifi.h inteface, it is up to the user to first call @ref
  *      wifi_connect_network() to connect to the wifi and set up a port.
  *
- * 2)   After the wifi has been enabled and one wants to play with other
+ * 2.   After the wifi has been enabled and one wants to play with other
  *      players, it is vital to announce this using @ref wifi_announce_lfg().
  *      One can optionally send this message periodically, but it must be sent
  *      at least once upon connecting to the network.
  *
- * 3)   The user is responsible for periodically polling incoming messages with
+ * 3.   The user is responsible for periodically polling incoming messages with
  *      @ref receive_messages(). Upon receiving an LFG msg, the receiver becomes
  *      the `master` and is in charge of syncing game (with commands such as
  *      RESET_GAME and IS_PLAY defined in @ref wifi_instr.h )
  *
- * 4)   During game, for every frame send !LOCAL! player  with @ref
+ * 4.   During game, for every frame send \b LOCAL player  with @ref
  *      send_status(Player* const plr) and the function will arbitrate whether
  *      or not a transmission needs to be sent or not.
  *
- * 5)   Whenever a player does damage, this is to be sent to the remote NDS
+ * 5.   Whenever a player does damage, this is to be sent to the remote NDS
  *      using @ref send_damage(u8 dmg_x, u8 dmg_y, u8 dmg)
  *
- * 6)   During the game, it is important to periodically poll for incoming
+ * 6.   During the game, it is important to periodically poll for incoming
  *       messages (again with @ref receive_messages()) and handle the (player,
  *      damage or abort) instructions.
  *
- * 7)   Whoever loses the game (health, giveup, whatever) is in charge of
+ * 7.   Whoever loses the game (health, giveup, whatever) is in charge of
  *      signalling the lose and becomes the new master, tasked with setting up a
  *      new round etc.
  *
- * 8)   To disconnect from wifi (i.e. single player) call @ref
+ * 8.   To disconnect from wifi (i.e. single player) call @ref
  *      wifi_disconnect_network()
  *
  */
