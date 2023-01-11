030ce5fecd1ad282eaae3ff128e943dca151ae13
Lokman <lokman.mheni@gmail.com>
2023-01-11 12:50:15 +0100

    attempts with sprites players

--- 00_project/source/graphics/graphics.c
+++ 00_project/source/graphics/graphics.c
@@ -193,20 +193,12 @@ void sprite_pos_local(Player* const player) {
 	//using extended palettes:
 	vramSetBankF(VRAM_F_LCD);
 	dmaCopy(playerPal, SPRITE_PALETTE , playerPalLen);
-	dmaCopy(player2Pal, &SPRITE_PALETTE[playerPalLen+1] , player2PalLen);
+	dmaCopy(player2Pal, &SPRITE_PALETTE[playerPalLen] , player2PalLen);
 	dmaCopy(playerPal, &VRAM_F_EXT_PALETTE[0], playerPalLen);
 	dmaCopy(player2Pal, &VRAM_F_EXT_PALETTE[1], player2PalLen);
 	dmaCopy(playerTiles, gfx, playerTilesLen);
 	dmaCopy(player2Tiles, gfx1, player2TilesLen);
 	vramSetBankF(VRAM_F_SPRITE_EXT_PALETTE);
-
-	// Copy data for the graphic (palette and bitmap)
-	//dmaCopy(playerPal, SPRITE_PALETTE , playerPalLen);
-
-
-	//dmaCopy(player2Pal, &SPRITE_PALETTE[playerPalLen] , player2PalLen);
-	//dmaCopy(player2Tiles, gfx1, player2TilesLen);
-
 
    while (1)
    {
