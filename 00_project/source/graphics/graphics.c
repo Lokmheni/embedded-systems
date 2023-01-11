1add46879713f95f8b5248c84511989ae1d0ed1d
Lokman <lokman.mheni@gmail.com>
2023-01-11 23:28:37 +0100

    colore cornice cambia quando cambia lo sfondo del timer

--- 00_project/source/graphics/graphics.c
+++ 00_project/source/graphics/graphics.c
@@ -30,21 +30,149 @@
 #define	SPRITE_WIDTH	32
 #define	SPRITE_HEIGHT	32
 
-#define	RED   ARGB16(1,31,0,0)
-#define	GREEN ARGB16(1,0,31,0)
-#define	BLUE  ARGB16(1,0,0,31)
+
+/************************
+ * Macros for the colors
+ ***********************/
+#define	RED ARGB16(1,31,0,0)
+#define GREEN ARGB16(1,0,31,0)
+#define	BLUE ARGB16(1,0,0,31)
+#define	YELLOW ARGB16(1,31,31,0)
+#define	LIGHT_BLUE ARGB16(1,0,31,31)
 #define	WHITE ARGB16(1,31,31,31)
 #define	BLACK ARGB16(1,0,0,0)
 
+// Different color tiles
+u8 TransTile[64] =
+{
+	0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0
+};
+u8 GreenTile[64] =
+{
+	1,1,1,1,1,1,1,1,
+	1,1,1,1,1,1,1,1,
+	1,1,1,1,1,1,1,1,
+	1,1,1,1,1,1,1,1,
+	1,1,1,1,1,1,1,1,
+	1,1,1,1,1,1,1,1,
+	1,1,1,1,1,1,1,1,
+	1,1,1,1,1,1,1,1
+};
 
-int min = 0, sec = 0, msec = 0, time_remaining, time_round = 1000*60*2; // 120 seconds for each round
+u8 BlueTile[64] =
+{
+	2,2,2,2,2,2,2,2,
+	2,2,2,2,2,2,2,2,
+	2,2,2,2,2,2,2,2,
+	2,2,2,2,2,2,2,2,
+	2,2,2,2,2,2,2,2,
+	2,2,2,2,2,2,2,2,
+	2,2,2,2,2,2,2,2,
+	2,2,2,2,2,2,2,2
+};
+
+u8 WhiteTile[64] =
+{
+	3,3,3,3,3,3,3,3,
+	3,3,3,3,3,3,3,3,
+	3,3,3,3,3,3,3,3,
+	3,3,3,3,3,3,3,3,
+	3,3,3,3,3,3,3,3,
+	3,3,3,3,3,3,3,3,
+	3,3,3,3,3,3,3,3,
+	3,3,3,3,3,3,3,3
+};
+
+u8 BlackTile[64] =
+{
+	4,4,4,4,4,4,4,4,
+	4,4,4,4,4,4,4,4,
+	4,4,4,4,4,4,4,4,
+	4,4,4,4,4,4,4,4,
+	4,4,4,4,4,4,4,4,
+	4,4,4,4,4,4,4,4,
+	4,4,4,4,4,4,4,4,
+	4,4,4,4,4,4,4,4
+};
+
+u8 RedTile[64] =
+{
+	5,5,5,5,5,5,5,5,
+	5,5,5,5,5,5,5,5,
+	5,5,5,5,5,5,5,5,
+	5,5,5,5,5,5,5,5,
+	5,5,5,5,5,5,5,5,
+	5,5,5,5,5,5,5,5,
+	5,5,5,5,5,5,5,5,
+	5,5,5,5,5,5,5,5,
+};
+
+int min = 0, sec = 0, msec = 0, time_remaining, time_round = 1000*20; // 120 seconds for each round
+
+int colore_cornice;
+
+void show_health(){
+	// 1) VRAM configuration for SUB engine
+	VRAM_C_CR = VRAM_ENABLE | VRAM_C_SUB_BG;
+	// 2) SUB engine configuration in tiled mode
+	REG_DISPCNT_SUB = MODE_0_2D | DISPLAY_BG0_ACTIVE;
+	// 3) Configure the background
+	BGCTRL_SUB[0] = BG_32x32 | BG_COLOR_256 | BG_MAP_BASE(0) | BG_TILE_BASE(1);
+	// 4) Copy the 4 tiles to the tile base
+
+	//swiCopy(TransTile, &BG_TILE_RAM_SUB(1)[0], 32);
+	swiCopy(GreenTile, &BG_TILE_RAM_SUB(1)[32], 32);
+	swiCopy(BlueTile, &BG_TILE_RAM_SUB(1)[64], 32);
+	swiCopy(WhiteTile, &BG_TILE_RAM_SUB(1)[96], 32);
+	swiCopy(BlackTile, &BG_TILE_RAM_SUB(1)[128], 32);
+	swiCopy(RedTile, &BG_TILE_RAM_SUB(1)[160], 32);
+	// 5) Initialize the palette (5 components)
+	//BG_PALETTE_SUB[0] = TRANS;
+	BG_PALETTE_SUB[1] = GREEN;
+	BG_PALETTE_SUB[2] = BLUE;
+	BG_PALETTE_SUB[3] = WHITE;
+	BG_PALETTE_SUB[4] = BLACK;
+	BG_PALETTE_SUB[5] = RED;
+	// 6) Generate the map
+	int i,j, tile;
+	/*	for(i = 0; i < 32; i++){
+			for(j = 0; j < 24; j++)
+				BG_MAP_RAM_SUB(25)[j*32+i] = 0;
+		}*/
+
+	//for(tile = 0; tile <1024; tile++)
+	//	BG_MAP_RAM_SUB(0)[tile] = 0;
+
+		for(j = 2; j < 6; j++){
+			if(j == 2 || j == 5){
+					for (i = 3; i < 14; i++)
+						BG_MAP_RAM_SUB(0)[j*32+i] = colore_cornice;
+					for (i = 19; i < 30; i++)
+						BG_MAP_RAM_SUB(0)[j*32+i] = colore_cornice;
+				}
+			else{
+				for(i = 3; i < 14; i++)
+					if(i == 3 || i == 13)
+						BG_MAP_RAM_SUB(0)[j*32+i] = colore_cornice;
+					else
+						BG_MAP_RAM_SUB(0)[j*32+i] = 1;
+				for(i = 19; i < 30; i++)
+					if(i == 19 || i == 29)
+						BG_MAP_RAM_SUB(0)[j*32+i] = colore_cornice;
+					else
+						BG_MAP_RAM_SUB(0)[j*32+i] = 1;
+			}
+		}
+	}
 
 void updateChrono(){
-	//int tile;
-
-	changeColorDisp(BLACK, RED);
-	//for(tile = 0; tile <1024; tile++)
-		//	BG_MAP_RAM_SUB(0)[tile] = 0;
 	updateChronoDisp(BG_MAP_RAM_SUB(0), min, sec, msec);
 	show_health();
 }
@@ -59,11 +187,15 @@ void ISR_TIMER0(){
 			if(sec == 0)
 				min++;
 		}
-		if(time_remaining < 10*1000)
+		if(time_remaining < 10*1000){
+			colore_cornice = 2;
 			//The background color is ste to BLACK and the digits color is set to RED
 			changeColorDisp(BLACK, RED);
-		else
+		}
+		else{
+			colore_cornice = 4;
 			changeColorDisp(WHITE, BLACK);
+		}
 		//updateChronoDisp(BG_MAP_RAM_SUB(0), min, sec, msec);
 	}
 	else
--- 00_project/source/main.c
+++ 00_project/source/main.c
@@ -20,89 +20,6 @@
 #include "graphics/chrono_display.h"
 #include "health.h"
 
-/************************
- * Macros for the colors
- ***********************/
-#define	RED ARGB16(1,31,0,0)
-#define GREEN ARGB16(1,0,31,0)
-#define	BLUE ARGB16(1,0,0,31)
-#define	YELLOW ARGB16(1,31,31,0)
-#define	LIGHT_BLUE ARGB16(1,0,31,31)
-#define	WHITE ARGB16(1,31,31,31)
-#define	BLACK ARGB16(1,0,0,0)
-
-
-// Different color tiles
-u8 TransTile[64] =
-{
-	0,0,0,0,0,0,0,0,
-	0,0,0,0,0,0,0,0,
-	0,0,0,0,0,0,0,0,
-	0,0,0,0,0,0,0,0,
-	0,0,0,0,0,0,0,0,
-	0,0,0,0,0,0,0,0,
-	0,0,0,0,0,0,0,0,
-	0,0,0,0,0,0,0,0
-};
-u8 GreenTile[64] =
-{
-	1,1,1,1,1,1,1,1,
-	1,1,1,1,1,1,1,1,
-	1,1,1,1,1,1,1,1,
-	1,1,1,1,1,1,1,1,
-	1,1,1,1,1,1,1,1,
-	1,1,1,1,1,1,1,1,
-	1,1,1,1,1,1,1,1,
-	1,1,1,1,1,1,1,1
-};
-
-u8 BlueTile[64] =
-{
-	2,2,2,2,2,2,2,2,
-	2,2,2,2,2,2,2,2,
-	2,2,2,2,2,2,2,2,
-	2,2,2,2,2,2,2,2,
-	2,2,2,2,2,2,2,2,
-	2,2,2,2,2,2,2,2,
-	2,2,2,2,2,2,2,2,
-	2,2,2,2,2,2,2,2
-};
-
-u8 WhiteTile[64] =
-{
-	3,3,3,3,3,3,3,3,
-	3,3,3,3,3,3,3,3,
-	3,3,3,3,3,3,3,3,
-	3,3,3,3,3,3,3,3,
-	3,3,3,3,3,3,3,3,
-	3,3,3,3,3,3,3,3,
-	3,3,3,3,3,3,3,3,
-	3,3,3,3,3,3,3,3
-};
-
-u8 BlackTile[64] =
-{
-	4,4,4,4,4,4,4,4,
-	4,4,4,4,4,4,4,4,
-	4,4,4,4,4,4,4,4,
-	4,4,4,4,4,4,4,4,
-	4,4,4,4,4,4,4,4,
-	4,4,4,4,4,4,4,4,
-	4,4,4,4,4,4,4,4,
-	4,4,4,4,4,4,4,4
-};
-
-u8 RedTile[64] =
-{
-	5,5,5,5,5,5,5,5,
-	5,5,5,5,5,5,5,5,
-	5,5,5,5,5,5,5,5,
-	5,5,5,5,5,5,5,5,
-	5,5,5,5,5,5,5,5,
-	5,5,5,5,5,5,5,5,
-	5,5,5,5,5,5,5,5,
-	5,5,5,5,5,5,5,5,
-};
 
 
 int main(void)
@@ -163,59 +80,7 @@ int main(void)
 }
 
 
-void show_health(){
-	// 1) VRAM configuration for SUB engine
-	VRAM_C_CR = VRAM_ENABLE | VRAM_C_SUB_BG;
-	// 2) SUB engine configuration in tiled mode
-	REG_DISPCNT_SUB = MODE_0_2D | DISPLAY_BG0_ACTIVE;
-	// 3) Configure the background
-	BGCTRL_SUB[0] = BG_32x32 | BG_COLOR_256 | BG_MAP_BASE(0) | BG_TILE_BASE(1);
-	// 4) Copy the 4 tiles to the tile base
 
-	//swiCopy(TransTile, &BG_TILE_RAM_SUB(1)[0], 32);
-	swiCopy(GreenTile, &BG_TILE_RAM_SUB(1)[32], 32);
-	swiCopy(BlueTile, &BG_TILE_RAM_SUB(1)[64], 32);
-	swiCopy(WhiteTile, &BG_TILE_RAM_SUB(1)[96], 32);
-	swiCopy(BlackTile, &BG_TILE_RAM_SUB(1)[128], 32);
-	swiCopy(RedTile, &BG_TILE_RAM_SUB(1)[160], 32);
-	// 5) Initialize the palette (5 components)
-	//BG_PALETTE_SUB[0] = TRANS;
-	BG_PALETTE_SUB[1] = GREEN;
-	BG_PALETTE_SUB[2] = BLUE;
-	BG_PALETTE_SUB[3] = WHITE;
-	BG_PALETTE_SUB[4] = BLACK;
-	BG_PALETTE_SUB[5] = RED;
-	// 6) Generate the map
-	int i,j, tile;
-	/*	for(i = 0; i < 32; i++){
-			for(j = 0; j < 24; j++)
-				BG_MAP_RAM_SUB(25)[j*32+i] = 0;
-		}*/
-
-	//for(tile = 0; tile <1024; tile++)
-	//	BG_MAP_RAM_SUB(0)[tile] = 0;
-
-		for(j = 2; j < 6; j++){
-			if(j == 2 || j == 5){
-					for (i = 3; i < 14; i++)
-						BG_MAP_RAM_SUB(0)[j*32+i] = 4;
-					for (i = 19; i < 30; i++)
-						BG_MAP_RAM_SUB(0)[j*32+i] = 4;
-				}
-			else{
-				for(i = 3; i < 14; i++)
-					if(i == 3 || i == 13)
-						BG_MAP_RAM_SUB(0)[j*32+i] = 4;
-					else
-						BG_MAP_RAM_SUB(0)[j*32+i] = 1;
-				for(i = 19; i < 30; i++)
-					if(i == 19 || i == 29)
-						BG_MAP_RAM_SUB(0)[j*32+i] = 4;
-					else
-						BG_MAP_RAM_SUB(0)[j*32+i] = 1;
-			}
-		}
-	}
 
 
 
