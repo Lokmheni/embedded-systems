a9d3fd6d9935d8a282f7be5eeb85c6f3ca32df78
Lokman <lokman.mheni@gmail.com>
2023-01-19 17:51:46 +0100

    ättempts

--- 00_project/source/graphics/graphics.c
+++ 00_project/source/graphics/graphics.c
@@ -264,24 +264,24 @@ void show_health(const Player *t, const Player* s){
 	}
 	else if((s->health) > 30){
 		for(j = 2; j < 5; j++){
-			BG_MAP_RAM_SUB(1)[j*32+29] = 1;
-			BG_MAP_RAM_SUB(1)[j*32+28] = 1;
-			BG_MAP_RAM_SUB(1)[j*32+27] = 1;
-			BG_MAP_RAM_SUB(1)[j*32+26] = 1;
+			BG_MAP_RAM_SUB(1)[j*32+29] = 6;
+			BG_MAP_RAM_SUB(1)[j*32+28] = 6;
+			BG_MAP_RAM_SUB(1)[j*32+27] = 6;
+			BG_MAP_RAM_SUB(1)[j*32+26] = 6;
 		}
 	}
 
 	else if((s->health) > 20){
 		for(j = 2; j < 5; j++){
-			BG_MAP_RAM_SUB(1)[j*32+29] = 1;
-			BG_MAP_RAM_SUB(1)[j*32+28] = 1;
-			BG_MAP_RAM_SUB(1)[j*32+27] = 1;
+			BG_MAP_RAM_SUB(1)[j*32+29] = 6;
+			BG_MAP_RAM_SUB(1)[j*32+28] = 6;
+			BG_MAP_RAM_SUB(1)[j*32+27] = 6;
 		}
 	}
 	else if((s->health) > 10){
 		for(j = 2; j < 5; j++){
-			BG_MAP_RAM_SUB(1)[j*32+29] = 6;
-			BG_MAP_RAM_SUB(1)[j*32+28] = 6;
+			BG_MAP_RAM_SUB(1)[j*32+29] = 5;
+			BG_MAP_RAM_SUB(1)[j*32+28] = 5;
 		}
 	}
 
@@ -364,24 +364,24 @@ void show_health(const Player *t, const Player* s){
 	}
 	else if((t->health) > 30){
 		for(j = 2; j < 5; j++){
-			BG_MAP_RAM_SUB(1)[j*32+6] = 1;
-			BG_MAP_RAM_SUB(1)[j*32+5] = 1;
-			BG_MAP_RAM_SUB(1)[j*32+4] = 1;
-			BG_MAP_RAM_SUB(1)[j*32+3] = 1;
+			BG_MAP_RAM_SUB(1)[j*32+6] = 6;
+			BG_MAP_RAM_SUB(1)[j*32+5] = 6;
+			BG_MAP_RAM_SUB(1)[j*32+4] = 6;
+			BG_MAP_RAM_SUB(1)[j*32+3] = 6;
 		}
 	}
 
 	else if((t->health) > 20){
 		for(j = 2; j < 5; j++){
-			BG_MAP_RAM_SUB(1)[j*32+3] = 1;
-			BG_MAP_RAM_SUB(1)[j*32+4] = 1;
-			BG_MAP_RAM_SUB(1)[j*32+5] = 1;
+			BG_MAP_RAM_SUB(1)[j*32+3] = 6;
+			BG_MAP_RAM_SUB(1)[j*32+4] = 6;
+			BG_MAP_RAM_SUB(1)[j*32+5] = 6;
 		}
 	}
 	else if((t->health) > 10){
 		for(j = 2; j < 5; j++){
-			BG_MAP_RAM_SUB(1)[j*32+3] = 6;
-			BG_MAP_RAM_SUB(1)[j*32+4] = 6;
+			BG_MAP_RAM_SUB(1)[j*32+3] = 5;
+			BG_MAP_RAM_SUB(1)[j*32+4] = 5;
 		}
 	}
 
@@ -526,13 +526,18 @@ void show_settings(int games_played, int games_won){
 	//changeColorDisp(WHITE , BLACK);
 
 	// Configure the SUB engine in Rotoscale Mode
-	REG_DISPCNT_SUB = MODE_5_2D | DISPLAY_BG2_ACTIVE;// | DISPLAY_BG1_ACTIVE;
+	REG_DISPCNT_SUB = MODE_5_2D | DISPLAY_BG2_ACTIVE; //| DISPLAY_BG1_ACTIVE;
 	//VRAM_H_CR = VRAM_ENABLE | VRAM_H_SUB_BG;
-	BGCTRL_SUB[2] = BG_BMP_BASE(0) | BG_BMP8_256x256;
-	swiCopy(statisticsBitmap, BG_GFX_SUB, statisticsBitmapLen/2);
+	BGCTRL_SUB[2] = BG_BMP_BASE(1) | BG_BMP8_256x256;
+	swiCopy(statisticsBitmap, &BG_GFX_SUB[1], statisticsBitmapLen/2);
 	swiCopy(statisticsPal, BG_PALETTE_SUB, statisticsPalLen/2);
 
+	//Configure the engine to be used as a 32x32 grid of tiles of 256 colors
+	//BGCTRL_SUB[1] = BG_32x32 | BG_COLOR_256 | BG_MAP_BASE(0) | BG_TILE_BASE(1);
 
+	//Copy the tiles and the palette to the corresonding location
+	//swiCopy(numbersTiles, BG_TILE_RAM_SUB(1), numbersTilesLen);
+	//swiCopy(numbersPal, BG_PALETTE_SUB, numbersPalLen);
 	//REG_DISPCNT_SUB = MODE_5_2D | DISPLAY_BG1_ACTIVE; //| DISPLAY_BG3_ACTIVE;
 	// Configure background BG3 in rotoscale mode using 8bit pixels
 	//BGCTRL_SUB[1] = BG_32x32 | BG_COLOR_256 | BG_MAP_BASE(0) | BG_TILE_BASE(1);
@@ -543,9 +548,9 @@ void show_settings(int games_played, int games_won){
 	int games_played_units = (games_played%100)%10;
 
 	//changeColorDisp(WHITE, BLACK);
-	printDigit(BG_MAP_RAM_SUB(0) , games_played_hundreds, 8, 8);
+	/*printDigit(BG_MAP_RAM_SUB(0) , games_played_hundreds, 8, 8);
 	printDigit(BG_MAP_RAM_SUB(0) , games_played_dozens, 4, 0);
-	printDigit(BG_MAP_RAM_SUB(0) , games_played_units, 8, 0);
+	printDigit(BG_MAP_RAM_SUB(0) , games_played_units, 8, 0);*/
 }
 
 void sprite_pos_local(const Player*  player) {
